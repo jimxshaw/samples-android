a88d6cb087b391f98ceb42415ba2edd875094571
Jim Shaw <jimxshaw@gmail.com>
2016-03-15 15:40:19 -0500

    added case scenario to onOptionsItemSelected switch statement for an add a task alert dialog

--- ToDoList/app/src/main/java/org/guildsa/todolist/MainActivity.java
+++ ToDoList/app/src/main/java/org/guildsa/todolist/MainActivity.java
@@ -1,10 +1,13 @@
 package org.guildsa.todolist;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 // MainActivity inherits from many super classes of which one is AppCompatActivity. The highest
 // super class Activity is one where all sub activity classes inherit.
@@ -22,17 +25,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.main);
     }
 
+    // This method is used to add a task menu in the top right corner.
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
 
+    // This method is to get a selected item from the task menu.
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_add_task:
-                Log.d("MainActivity", "Add a new task");
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Add a task");
+                builder.setMessage("What do you want to do?");
+                final EditText inputField = new EditText(this);
+                builder.setView(inputField);
+                builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Log.d("MainActivity", inputField.getText().toString());
+                    }
+                });
+
+                builder.setNegativeButton("Cancel", null);
+
+                builder.create().show();
+
                 return true;
 
             default:
