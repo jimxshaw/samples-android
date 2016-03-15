ca4fae5233da36ece069895b9c47d8671d7ed234
Jim Shaw <jimxshaw@gmail.com>
2016-03-15 17:21:16 -0500

    added TaskDBHelper class

---
+++ ToDoList/app/src/main/java/org/guildsa/todolist/db/TaskDBHelper.java
@@ -0,0 +1,30 @@
+package org.guildsa.todolist.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+// This helper class will open the database for us.
+public class TaskDBHelper extends SQLiteOpenHelper {
+    public TaskDBHelper(Context context) {
+        super(context, TaskContract.DB_NAME, null, TaskContract.DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqlDB) {
+        String sqlQuery = String.format("CREATE TABLE %s (" +
+                                        "_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                                        "%s TEXT)", TaskContract.TABLE, TaskContract.Columns.TASK);
+
+        Log.d("TaskDBHelper", "Query to form table: " + sqlQuery);
+
+        sqlDB.execSQL(sqlQuery);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqlDB, int i, int i2) {
+        sqlDB.execSQL("DROP TABLE IF EXISTS " + TaskContract.TABLE);
+        onCreate(sqlDB);
+    }
+}
