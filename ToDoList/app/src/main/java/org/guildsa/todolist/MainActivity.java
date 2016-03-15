203573753bd9f39abad8d07c6713edbe944e64cf
James Shaw <jamesshaw@Jamess-MacBook-Pro.local>
2016-03-15 13:33:37 -0500

    added todolist boilerplate

---
+++ ToDoList/app/src/main/java/org/guildsa/todolist/MainActivity.java
@@ -0,0 +1,19 @@
+package org.guildsa.todolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// MainActivity inherits from many super classes of which one is AppCompatActivity. The highest
+// super class Activity is one where all sub activity classes inherit.
+public class MainActivity extends AppCompatActivity {
+    // The @Override Android Studio annotation is not mandatory but is quite useful to use anyway.
+    // Its purpose is to tell the compiler that we're overriding a method from a class we're
+    // inheriting from. Once the compiler knows this, it can help us identify errors in our code.
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
