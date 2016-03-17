6210a0767505acf2fa3d23b01c9f0e8ea223f63a
Jim Shaw <jimxshaw@gmail.com>
2016-03-17 15:02:56 -0500

    added comments

--- weatherapp/app/src/main/java/guildsa/org/weatherapp/data/Item.java
+++ weatherapp/app/src/main/java/guildsa/org/weatherapp/data/Item.java
@@ -2,8 +2,28 @@ package guildsa.org.weatherapp.data;
 
 import org.json.JSONObject;
 
+/*
+    The Yahoo Weather API JSON object with the key-value pairs we need essentially look like this:
+
+    "channel": {
+        "units": { "temperature": "F"
+        },
+        "item": { "condition": {
+                        "code": "33",
+                        "temp": "-9",
+                        "text": "Fair"
+                    }
+        }
+    }
+
+ */
+
 public class Item implements JSONPopulator {
     private Condition condition;
+
+    public Condition getCondition() {
+        return condition;
+    }
 
     @Override
     public void populate(JSONObject data) {
--- weatherapp/app/src/main/java/guildsa/org/weatherapp/data/Units.java
+++ weatherapp/app/src/main/java/guildsa/org/weatherapp/data/Units.java
@@ -2,6 +2,22 @@ package guildsa.org.weatherapp.data;
 
 import org.json.JSONObject;
 
+/*
+    The Yahoo Weather API JSON object with the key-value pairs we need essentially look like this:
+
+    "channel": {
+        "units": { "temperature": "F"
+        },
+        "item": { "condition": {
+                        "code": "33",
+                        "temp": "-9",
+                        "text": "Fair"
+                    }
+        }
+    }
+
+ */
+
 public class Units implements JSONPopulator {
     private String temperature;
 
