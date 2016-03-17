9b68b6c9858f78fdfa4d8e7d42c0a50bbf1692a6
Jim Shaw <jimxshaw@gmail.com>
2016-03-17 15:32:58 -0500

    implemented refreshWeather's async task doInBackground method

--- weatherapp/app/src/main/java/guildsa/org/weatherapp/service/YahooWeatherService.java
+++ weatherapp/app/src/main/java/guildsa/org/weatherapp/service/YahooWeatherService.java
@@ -1,10 +1,20 @@
 package guildsa.org.weatherapp.service;
 
+import android.net.Uri;
 import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class YahooWeatherService {
     private WeatherServiceCallback callback;
     private String location;
+    private Exception error;
 
     public YahooWeatherService(WeatherServiceCallback callback) {
         this.callback = callback;
@@ -15,10 +25,40 @@ public class YahooWeatherService {
     }
 
     public void refreshWeather(String location) {
+        final String queryLocation = location;
+
         new AsyncTask<String, Void, String>() {
 
             @Override
             protected String doInBackground(String... params) {
+                // Yahoo Query Language query take from Yahoo Weather API site.
+                String YQL = String.format("select * from weather.forecast where woeid in (select woeid from geo.places(1) where text=\"%s\")", queryLocation);
+
+                // Use the Yahoo Weather API endpoint but replace the default q= with %s that utilizes our YQL.
+                String endpoint = String.format("https://query.yahooapis.com/v1/public/yql?q=%s&format=json", Uri.encode(YQL));
+
+                try {
+                    URL url = new URL(endpoint);
+
+                    URLConnection connection = url.openConnection();
+
+                    InputStream inputStream = connection.getInputStream();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                    StringBuilder result = new StringBuilder();
+
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+
+                    return result.toString();
+                }
+                catch (Exception e) {
+                    error = e;
+                }
+
                 return null;
             }
 
