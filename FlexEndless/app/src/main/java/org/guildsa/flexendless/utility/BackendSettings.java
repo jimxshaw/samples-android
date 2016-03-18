7d5d99bf6a67ebc9f8e9192f9b47feffd384718f
Jim Shaw <jimxshaw@gmail.com>
2016-03-18 15:45:12 -0500

    added flexendless boilerplate project

---
+++ FlexEndless/app/src/main/java/org/guildsa/flexendless/utility/BackendSettings.java
@@ -0,0 +1,10 @@
+package org.guildsa.flexendless.utility;
+
+/**
+ * Here are the values used to communicate with the backend.
+ */
+public class BackendSettings {
+    public static final String APPLICATION_ID = "11F85AEF-6823-EF0F-FF7E-FE51467CA200";
+    public static final String ANDROID_SECRET_KEY = "E8783BC6-C55B-1372-FF77-450594223B00";
+    public static final String VERSION = "v1";
+}
