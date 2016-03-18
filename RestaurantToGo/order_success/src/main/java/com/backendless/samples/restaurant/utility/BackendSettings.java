5d2e9031827573d3d93a4f98b63f3cfc349f98e7
Jim Shaw <jimxshaw@gmail.com>
2016-03-18 13:53:31 -0500

    added backendless boilerplate project

---
+++ RestaurantToGo/order_success/src/main/java/com/backendless/samples/restaurant/utility/BackendSettings.java
@@ -0,0 +1,11 @@
+package com.backendless.samples.restaurant.utility;
+
+/**
+ * Provides values used to communicate with backend.
+ */
+public class BackendSettings
+{
+  public static final String APPLICATION_ID = "48FB63F1-E74D-4FBD-FFFA-B7A37FC57100";
+  public static final String ANDROID_SECRET_KEY = "D6366EE0-D872-2816-FFAB-A6BC12C4CC00";
+  public static final String VERSION = "v1";
+}
