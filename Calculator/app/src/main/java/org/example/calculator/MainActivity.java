01aec94ca22facefc267baf23581d1ef6e4cfae9
Jim Shaw <jimxshaw@gmail.com>
2016-03-20 20:53:32 -0500

    created on click event handlers for add, subtract, multiple and divide buttons

--- Calculator/app/src/main/java/org/example/calculator/MainActivity.java
+++ Calculator/app/src/main/java/org/example/calculator/MainActivity.java
@@ -8,8 +8,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText operand1;
+    private EditText operand2;
+    private Button btnAddition;
+    private Button btnSubtraction;
+    private Button btnMultiplication;
+    private Button btnDivision;
+    private TextView txtResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,59 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        operand1 = (EditText) findViewById(R.id.editOperand1);
+        operand2 = (EditText) findViewById(R.id.editOperand2);
+        btnAddition = (Button) findViewById(R.id.btnAddition);
+        btnSubtraction = (Button) findViewById(R.id.btnSubtraction);
+        btnMultiplication = (Button) findViewById(R.id.btnMultiplication);
+        btnDivision = (Button) findViewById(R.id.btnDivision);
+        txtResult = (TextView) findViewById(R.id.txtResult);
+
+        btnAddition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double oper1 = Double.parseDouble(operand1.getText().toString());
+                double oper2 = Double.parseDouble(operand2.getText().toString());
+
+                double theResult = oper1 + oper2;
+                txtResult.setText(Double.toString(theResult));
+            }
+        });
+
+        btnSubtraction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double oper1 = Double.parseDouble(operand1.getText().toString());
+                double oper2 = Double.parseDouble(operand2.getText().toString());
+
+                double theResult = oper1 - oper2;
+                txtResult.setText(Double.toString(theResult));
+            }
+        });
+
+        btnMultiplication.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double oper1 = Double.parseDouble(operand1.getText().toString());
+                double oper2 = Double.parseDouble(operand2.getText().toString());
+
+                double theResult = oper1 * oper2;
+                txtResult.setText(Double.toString(theResult));
+            }
+        });
+
+        btnDivision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double oper1 = Double.parseDouble(operand1.getText().toString());
+                double oper2 = Double.parseDouble(operand2.getText().toString());
+
+                double theResult = oper1 / oper2;
+                txtResult.setText(Double.toString(theResult));
+            }
+        });
+
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
