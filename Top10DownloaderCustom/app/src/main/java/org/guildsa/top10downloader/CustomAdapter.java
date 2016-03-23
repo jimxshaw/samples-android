80e70d7995db75bda03220b7a6f0b953b651116f
Jim Shaw <jimxshaw@gmail.com>
2016-03-23 14:15:03 -0500

    finished CustomAdapter.java by adding async task DownloadImageTask class. Used the class to pull the app icon .png file by image url and assigned the .png to our list item ImageView

--- Top10DownloaderCustom/app/src/main/java/org/guildsa/top10downloader/CustomAdapter.java
+++ Top10DownloaderCustom/app/src/main/java/org/guildsa/top10downloader/CustomAdapter.java
@@ -1,7 +1,10 @@
 package org.guildsa.top10downloader;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +13,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 
 public class CustomAdapter extends ArrayAdapter<Application> {
@@ -30,15 +34,47 @@ public class CustomAdapter extends ArrayAdapter<Application> {
 
         // Grab the views we want by their ids and cast them into the appropriate types.
         TextView app = (TextView) rowView.findViewById(R.id.app);
-        ImageView image = (ImageView) rowView.findViewById(R.id.image);
+
+        //ImageView image = (ImageView) rowView.findViewById(R.id.image);
 
         // Retrieve each application from our array list of applications according to its position.
         Application rawApplication = values.get(position);
 
         String appImageUrl = rawApplication.getImageUrl();
 
+        new DownloadImageTask((ImageView) rowView.findViewById(R.id.image)).execute(appImageUrl);
+
         app.setText(rawApplication.toString());
 
         return rowView;
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urlDisplay = urls[0];
+            Bitmap appImage = null;
+
+            try {
+                InputStream inputStream = new java.net.URL(urlDisplay).openStream();
+                appImage = BitmapFactory.decodeStream(inputStream);
+            }
+            catch (Exception e) {
+                Log.e("ImageUrl", e.getMessage());
+                e.printStackTrace();
+            }
+
+            return appImage;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+
     }
 }
