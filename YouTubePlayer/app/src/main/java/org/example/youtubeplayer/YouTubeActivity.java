8c5dbf40d2734a4a2350dd172a24c51b298982b8
Jim Shaw <jimxshaw@gmail.com>
2016-03-24 12:51:39 -0500

    added boilerplate implementation for YouTubePlayer.OnInitializedListener interface

--- YouTubePlayer/app/src/main/java/org/example/youtubeplayer/YouTubeActivity.java
+++ YouTubePlayer/app/src/main/java/org/example/youtubeplayer/YouTubeActivity.java
@@ -1,14 +1,18 @@
 package org.example.youtubeplayer;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 public class YouTubeActivity extends YouTubeBaseActivity
                              implements YouTubePlayer.OnInitializedListener
 {
 
+    // The google api key is given to us after we sign up for its api service.
     private String GOOGLE_API_KEY = "TBA";
     // Every video has an id, an unique identifier.
     private String YOUTUBE_VIDEO_ID = "TBA";
@@ -17,6 +21,14 @@ public class YouTubeActivity extends YouTubeBaseActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_youtube);
+
+        YouTubePlayerView youTubePlayerView = (YouTubePlayerView) findViewById(R.id.youtube_player);
+
+        // The initialize method takes in the google api key and a callback that will execute when
+        // this particular view starts up. Depending on whether or not the initialization is successful,
+        // this keyword will kick off onInitializationSuccess or onInitializationFailure.
+        youTubePlayerView.initialize(GOOGLE_API_KEY, this);
+
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 
@@ -30,4 +42,83 @@ public class YouTubeActivity extends YouTubeBaseActivity
 //        });
     }
 
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean wasRestored) {
+        Toast.makeText(this, "Initialized YouTube Player successfully", Toast.LENGTH_LONG).show();
+
+        // Set the listeners for the various events that the YouTube Player will send to us when we play
+        // videos or attempt to play, stop, start videos etc. The set methods take in their appropriate
+        // listener objects that are implemented below.
+        youTubePlayer.setPlayerStateChangeListener(playerStateChangeListener);
+        youTubePlayer.setPlaybackEventListener(playbackEventListener);
+
+        // The wasRestored parameter determines true or false the current session was a prior restored session.
+        if (!wasRestored) {
+            youTubePlayer.cueVideo(YOUTUBE_VIDEO_ID);
+        }
+    }
+
+    private YouTubePlayer.PlaybackEventListener playbackEventListener = new YouTubePlayer.PlaybackEventListener() {
+        @Override
+        public void onPlaying() {
+            Toast.makeText(YouTubeActivity.this, "Your video is playing", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onPaused() {
+            Toast.makeText(YouTubeActivity.this, "Your video is paused", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onStopped() {
+
+        }
+
+        @Override
+        public void onBuffering(boolean b) {
+
+        }
+
+        @Override
+        public void onSeekTo(int i) {
+
+        }
+    };
+
+    private YouTubePlayer.PlayerStateChangeListener playerStateChangeListener = new YouTubePlayer.PlayerStateChangeListener() {
+        @Override
+        public void onLoading() {
+
+        }
+
+        @Override
+        public void onLoaded(String s) {
+
+        }
+
+        @Override
+        public void onAdStarted() {
+            Toast.makeText(YouTubeActivity.this, "Click the Ad now!", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onVideoStarted() {
+            Toast.makeText(YouTubeActivity.this, "Your video has started", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onVideoEnded() {
+
+        }
+
+        @Override
+        public void onError(YouTubePlayer.ErrorReason errorReason) {
+
+        }
+    };
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        Toast.makeText(this, "Failed to initialized YouTube Player", Toast.LENGTH_LONG).show();
+    }
 }
