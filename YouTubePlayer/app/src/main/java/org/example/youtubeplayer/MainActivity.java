331236afb54333d4b99d4041671143d0c8c80646
Jim Shaw <jimxshaw@gmail.com>
2016-03-25 16:21:03 -0500

    misc tweaks

--- YouTubePlayer/app/src/main/java/org/example/youtubeplayer/MainActivity.java
+++ YouTubePlayer/app/src/main/java/org/example/youtubeplayer/MainActivity.java
@@ -1,5 +1,6 @@
 package org.example.youtubeplayer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+                          implements View.OnClickListener
+{
+
+    private Button btnPlaySingle;
+    private Button btnPlayStandalone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +24,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        btnPlaySingle = (Button) findViewById(R.id.btnPlaySingle);
+        btnPlayStandalone = (Button) findViewById(R.id.btnStandalone);
+
+        btnPlaySingle.setOnClickListener(this);
+        btnPlayStandalone.setOnClickListener(this);
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -48,5 +61,28 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+
+        // MainActivity does not have any YouTube api code because our YouTubeActivity and
+        // StandAloneActivity already do that. Instead of repeating ourselves, we'll simply
+        // instantiate a new intent that utilize our other activities to connect to YouTube.
+        switch(v.getId()) {
+            case R.id.btnPlaySingle:
+                // Intent(Context packageContext, Class<?> cls)
+                intent = new Intent(MainActivity.this, YouTubeActivity.class);
+                break;
+            case R.id.btnStandalone:
+                intent = new Intent(MainActivity.this, StandAloneActivity.class);
+                break;
+            default:
+        }
+
+        if (intent != null) {
+            startActivity(intent);
+        }
     }
 }
