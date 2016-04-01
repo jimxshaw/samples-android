89080b92ac836169d1376d1137d001f4b13dda34
James <jimxshaw@gmail.com>
2016-04-01 12:10:05 -0500

    modified QuizActivity's onSaveInstanceState to retain the value of whether or not the answer has been reveal for a particular question

--- GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -27,6 +27,7 @@ public class QuizActivity extends AppCompatActivity {
 
     private static final String TAG = "QuizActivity";
     private static final String KEY_INDEX = "index";
+    private static final String KEY_ISCHEATER = "isCheater";
 
     private static final int REQUEST_CODE_CHEAT = 0;
 
@@ -80,6 +81,7 @@ public class QuizActivity extends AppCompatActivity {
         super.onSaveInstanceState(savedInstanceState);
         Log.i(TAG, "onSaveInstanceState");
         savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+        savedInstanceState.putBoolean(KEY_ISCHEATER, mIsCheater);
     }
 
     @Override
@@ -193,6 +195,7 @@ public class QuizActivity extends AppCompatActivity {
 
         if (savedInstanceState != null) {
             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+            mIsCheater = savedInstanceState.getBoolean(KEY_ISCHEATER, false);
         }
 
         updateQuestion();
