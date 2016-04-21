66d85c684fc3ee7605653c249ef309c5388042d8
James <jimxshaw@gmail.com>
2016-04-21 16:21:10 -0500

    added colors to styles.xml. added basic app structure to activity_main.xml. captured layout views in MainActivity.java

--- MaterialDesignLibrary/1-Base/src/main/java/com.example.android.materialdesigncodelab/MainActivity.java
+++ MaterialDesignLibrary/1-Base/src/main/java/com.example.android.materialdesigncodelab/MainActivity.java
@@ -17,7 +17,9 @@
 package com.example.android.materialdesigncodelab;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -27,6 +29,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Adding the Toolbar to the Main Screen
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("Tab 1"));
+        tabs.addTab(tabs.newTab().setText("Tab 2"));
+        tabs.addTab(tabs.newTab().setText("Tab 3"));
     }
 
     @Override
