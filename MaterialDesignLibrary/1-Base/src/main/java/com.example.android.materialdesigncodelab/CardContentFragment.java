b93216cb3e9e323d8696d0c8db31082d1deb0cb3
Jim Shaw <jimxshaw@gmail.com>
2016-04-22 10:53:29 -0500

    implemented boilerplate fragment classes

--- MaterialDesignLibrary/1-Base/src/main/java/com.example.android.materialdesigncodelab/CardContentFragment.java
+++ MaterialDesignLibrary/1-Base/src/main/java/com.example.android.materialdesigncodelab/CardContentFragment.java
@@ -1,7 +1,14 @@
 package com.example.android.materialdesigncodelab;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class CardContentFragment extends Fragment {
-
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.item_card, null);
+    }
 }
--- MaterialDesignLibrary/1-Base/src/main/java/com.example.android.materialdesigncodelab/ListContentFragment.java
+++ MaterialDesignLibrary/1-Base/src/main/java/com.example.android.materialdesigncodelab/ListContentFragment.java
@@ -1,8 +1,15 @@
 package com.example.android.materialdesigncodelab;
 
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class ListContentFragment extends Fragment {
-
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.item_list, null);
+    }
 }
