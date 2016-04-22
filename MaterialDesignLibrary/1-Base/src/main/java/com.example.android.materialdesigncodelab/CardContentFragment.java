3430138d0c8bdc693c5cc03e22a3a521c1109c4d
Jim Shaw <jimxshaw@gmail.com>
2016-04-22 11:35:59 -0500

    added recycler_view layout, added static ViewHolder class to each fragment

--- MaterialDesignLibrary/1-Base/src/main/java/com.example.android.materialdesigncodelab/CardContentFragment.java
+++ MaterialDesignLibrary/1-Base/src/main/java/com.example.android.materialdesigncodelab/CardContentFragment.java
@@ -2,6 +2,7 @@ package com.example.android.materialdesigncodelab;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,5 +11,11 @@ public class CardContentFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.item_card, null);
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.item_card, parent, false));
+        }
     }
 }
--- MaterialDesignLibrary/1-Base/src/main/java/com.example.android.materialdesigncodelab/ListContentFragment.java
+++ MaterialDesignLibrary/1-Base/src/main/java/com.example.android.materialdesigncodelab/ListContentFragment.java
@@ -3,6 +3,7 @@ package com.example.android.materialdesigncodelab;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,5 +12,11 @@ public class ListContentFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.item_list, null);
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.item_list, parent, false));
+        }
     }
 }
