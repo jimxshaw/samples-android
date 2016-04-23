e02459fa01e73c02e0769e99faf8a001fdb46984
Jim Shaw <jimxshaw@gmail.com>
2016-04-23 13:38:40 -0500

    added some basic themes and colors

--- MaterialDesignFundamentals/ApplyTheme/app/src/main/java/me/jimmyshaw/applytheme/MainActivity.java
+++ MaterialDesignFundamentals/ApplyTheme/app/src/main/java/me/jimmyshaw/applytheme/MainActivity.java
@@ -1,5 +1,6 @@
 package me.jimmyshaw.applytheme;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Check if we're running on Android 5.0 or higher.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            // Call some material design APIs here.
+        }
+        else {
+            // For API 21 or below, implement this feature without material design.
+        }
     }
 }
