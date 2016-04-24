11a9b3e364594be4acd97c6fdea59ea2f5b3b4d0
Jim Shaw <jimxshaw@gmail.com>
2016-04-23 22:07:34 -0500

    added methods to customize snackbars

--- MaterialDesignFundamentals/DesignSupport/app/src/main/java/me/jimmyshaw/designsupport/MainActivity.java
+++ MaterialDesignFundamentals/DesignSupport/app/src/main/java/me/jimmyshaw/designsupport/MainActivity.java
@@ -1,5 +1,6 @@
 package me.jimmyshaw.designsupport;
 
+import android.graphics.Color;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,7 +8,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         Snackbar.make(mCoordinatorLayout, "Hello from simple snackbar!", Snackbar.LENGTH_SHORT).show();
     }
 
-    public void showSnackBarWithActionCallback(View view) {
+    public void showSnackbarWithActionCallback(View view) {
         Snackbar snackbar = Snackbar.make(mCoordinatorLayout, "Error loading file", Snackbar.LENGTH_SHORT);
         snackbar.setAction("RETRY", new View.OnClickListener() {
             @Override
@@ -44,5 +49,30 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(mCoordinatorLayout, "File loaded successfully!", Snackbar.LENGTH_SHORT).show();
             }
         }).show();
+    }
+
+    public void showSnackbarWithColoredText(View view) {
+        Snackbar snackbar = Snackbar.make(mCoordinatorLayout, "Download failed", Snackbar.LENGTH_SHORT);
+        snackbar.setAction("RETRY", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        }).show();
+
+        // Capture the snackbar's view and then set its new background color.
+        View snackbarLayout = snackbar.getView();
+        snackbarLayout.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+
+        // Set snackbar message text color.
+        TextView textViewMessage = (TextView) snackbarLayout.findViewById(android.support.design.R.id.snackbar_text);
+        textViewMessage.setTextColor(getResources().getColor(R.color.windowBackground));
+
+        // Change the snackbar action button text color - method 1.
+        //snackbar.setActionTextColor(getResources().getColor(R.color.colorPrimary));
+
+        // Change the snackbar action button text color - method 2.
+        Button snackbarButton = (Button) snackbarLayout.findViewById(android.support.design.R.id.snackbar_action);
+        snackbarButton.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
     }
 }
