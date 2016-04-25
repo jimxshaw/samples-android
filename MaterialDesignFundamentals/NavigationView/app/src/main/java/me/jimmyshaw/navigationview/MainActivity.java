25f075b24253279899e8ff676df86e0f96aacfc6
Jim Shaw <jimxshaw@gmail.com>
2016-04-24 22:14:22 -0500

    implemented nav view code in MainActivity.java

--- MaterialDesignFundamentals/NavigationView/app/src/main/java/me/jimmyshaw/navigationview/MainActivity.java
+++ MaterialDesignFundamentals/NavigationView/app/src/main/java/me/jimmyshaw/navigationview/MainActivity.java
@@ -1,13 +1,38 @@
 package me.jimmyshaw.navigationview;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private TextView mTextViewMenuItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mTextViewMenuItem = (TextView) findViewById(R.id.textview_menu_item);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        // ActionBarDrawerToggle(Activity activity, DrawerLayout drawerLayout, Toolbar toolbar,
+        // @StringRes int openDrawerContentDescRes,
+        // @StringRes int closeDrawerContentDescRes)
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar,
+                R.string.drawer_open, R.string.drawer_close);
+
+        mDrawerLayout.setDrawerListener(drawerToggle);
+        drawerToggle.syncState();
     }
 }
