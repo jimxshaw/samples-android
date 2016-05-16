405a22177d212a5d3f306021e07b3d78372f168d
Jim Shaw <jimxshaw@gmail.com>
2016-05-16 14:43:15 -0500

    added draw method in Mesh class

--- OpenGL/OpenGLESIntro/app/src/main/java/me/jimmyshaw/openglesintro/Mesh.java
+++ OpenGL/OpenGLESIntro/app/src/main/java/me/jimmyshaw/openglesintro/Mesh.java
@@ -5,6 +5,8 @@ import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 import java.nio.ShortBuffer;
 
+import javax.microedition.khronos.opengles.GL10;
+
 public class Mesh
 {
     // Translate params.
@@ -25,6 +27,35 @@ public class Mesh
     private float[] rgba = new float[]{1.0f, 1.0f, 1.0f, 1.0f};
 
     private FloatBuffer colorBuffer = null;
+
+    public void draw(GL10 gl)
+    {
+        gl.glFrontFace(GL10.GL_CCW);
+        // We enable the feature for OpenGL to not draw a face of a shape. Which face do we not
+        // want drawn? The back face.
+        gl.glEnable(GL10.GL_CULL_FACE);
+        gl.glCullFace(GL10.GL_BACK);
+        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+
+        gl.glVertexPointer(3, GL10.GL_FLOAT, 0, verticesBuffer);
+
+        gl.glColor4f(rgba[0], rgba[1], rgba[2], rgba[3]);
+
+        if (colorBuffer != null)
+        {
+            gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
+            gl.glColorPointer(4, GL10.GL_FLOAT, 0, colorBuffer);
+        }
+
+        gl.glTranslatef(x, y, z);
+        gl.glRotatef(rx, 1, 0, 0);
+        gl.glRotatef(ry, 0, 1, 0);
+        gl.glRotatef(rz, 0, 0, 1);
+
+        gl.glDrawElements(GL10.GL_TRIANGLES, numberOfIndices, GL10.GL_UNSIGNED_SHORT, indicesBuffer);
+        gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+        gl.glDisable(GL10.GL_CULL_FACE);
+    }
 
     // Method to set all of our vertices.
     protected void setVertices(float[] vertices)
