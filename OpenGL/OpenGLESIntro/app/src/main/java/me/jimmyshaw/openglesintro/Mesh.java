ed1b851ef6de2373142d46139423242bf3f9f99a
Jim Shaw <jimxshaw@gmail.com>
2016-05-16 16:00:08 -0500

    added loadBitmap and loadGLTexture methods to Mesh class

--- OpenGL/OpenGLESIntro/app/src/main/java/me/jimmyshaw/openglesintro/Mesh.java
+++ OpenGL/OpenGLESIntro/app/src/main/java/me/jimmyshaw/openglesintro/Mesh.java
@@ -1,5 +1,8 @@
 package me.jimmyshaw.openglesintro;
 
+import android.graphics.Bitmap;
+import android.opengl.GLUtils;
+
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
@@ -19,14 +22,23 @@ public class Mesh
     public float ry = 0;
     public float rz = 0;
 
+    // A field being set to -1 means it will be assigned to some other value later.
+    private int textureId = -1;
+
+    private Bitmap bitmap;
+
+    private boolean shouldLoadTexture = true;
+
     private FloatBuffer verticesBuffer = null;
     private ShortBuffer indicesBuffer = null;
 
-    private int numOfIndices = -1;
+    private int numberOfIndices = -1;
 
     private float[] rgba = new float[]{1.0f, 1.0f, 1.0f, 1.0f};
 
     private FloatBuffer colorBuffer = null;
+
+    private FloatBuffer textureBuffer;
 
     public void draw(GL10 gl)
     {
@@ -50,11 +62,20 @@ public class Mesh
         gl.glRotatef(ry, 0, 1, 0);
         gl.glRotatef(rz, 0, 0, 1);
 
-        gl.glDrawElements(GL10.GL_TRIANGLES, numOfIndices,
+        gl.glDrawElements(GL10.GL_TRIANGLES, numberOfIndices,
                 GL10.GL_UNSIGNED_SHORT, indicesBuffer);
 
         gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
         gl.glDisable(GL10.GL_CULL_FACE);
+    }
+
+    protected void setTextureCoordinates(float[] textureCoordinates)
+    {
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(textureCoordinates.length * 4);
+        byteBuffer.order(ByteOrder.nativeOrder());
+        textureBuffer = byteBuffer.asFloatBuffer();
+        textureBuffer.put(textureCoordinates);
+        textureBuffer.position(0);
     }
 
     protected void setVertices(float[] vertices)
@@ -77,7 +98,7 @@ public class Mesh
         indicesBuffer = ibb.asShortBuffer();
         indicesBuffer.put(indices);
         indicesBuffer.position(0);
-        numOfIndices = indices.length;
+        numberOfIndices = indices.length;
     }
 
     protected void setColor(float red, float green, float blue, float alpha)
@@ -86,5 +107,34 @@ public class Mesh
         rgba[1] = green;
         rgba[2] = blue;
         rgba[3] = alpha;
+    }
+
+    public void loadBitmap(Bitmap bitmap)
+    {
+        this.bitmap = bitmap;
+        shouldLoadTexture = true;
+    }
+
+    private void loadGLTexture(GL10 gl)
+    {
+        //Generate 1 texture pointer.
+        int[] texture = new int[1];
+        gl.glGenTextures(1, texture, 0);
+        textureId = texture[0];
+
+        // Bind texture pointer to our array.
+        gl.glBindTexture(GL10.GL_TEXTURE_2D, textureId);
+
+        // Create nearest filtered texture.
+        gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_LINEAR);
+        gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
+
+        // Different possible texture parameters, e.g. GL10.GL_CLAMP_TO_EDGE.
+        gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_S, GL10.GL_CLAMP_TO_EDGE);
+        gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_T, GL10.GL_REPEAT);
+
+        // Use the Android GLUtils to specify a two-dimensional texture image from our bitmap.
+        GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, bitmap, 0);
+
     }
 }
