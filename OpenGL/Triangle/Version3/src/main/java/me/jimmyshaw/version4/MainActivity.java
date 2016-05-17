9aefc4587dcc6264c0d0bff541f03a7eea98888d
James <jimxshaw@gmail.com>
2016-05-17 15:23:52 -0500

    added a matrix variable to vertexShaderCode. passed the transformation matrix in to the draw method. calculated the camera view with Matrix methods

--- OpenGL/Triangle/Version3/src/main/java/me/jimmyshaw/version4/MainActivity.java
+++ OpenGL/Triangle/Version3/src/main/java/me/jimmyshaw/version4/MainActivity.java
@@ -3,6 +3,7 @@ package me.jimmyshaw.version4;
 import android.content.Context;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -46,6 +47,10 @@ public class MainActivity extends AppCompatActivity
 
         private Triangle mTriangle;
 
+        private final float[] mMVPMatrix = new float[16];
+        private final float[] mProjectionMatrix = new float[16];
+        private final float[] mViewMatrix = new float[16];
+
         @Override
         public void onSurfaceCreated(GL10 gl, EGLConfig config)
         {
@@ -58,14 +63,18 @@ public class MainActivity extends AppCompatActivity
         public void onSurfaceChanged(GL10 gl, int width, int height)
         {
             GLES20.glViewport(0, 0, width, height);
+            float ratio = (float) width / height;
+            Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
         }
 
         @Override
         public void onDrawFrame(GL10 gl)
         {
+            Matrix.setLookAtM(mViewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+            Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
             GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 
-            mTriangle.draw();
+            mTriangle.draw(mMVPMatrix);
         }
     }
 }
--- OpenGL/Triangle/Version3/src/main/java/me/jimmyshaw/version4/Triangle.java
+++ OpenGL/Triangle/Version3/src/main/java/me/jimmyshaw/version4/Triangle.java
@@ -11,8 +11,9 @@ public class Triangle
 
     private final String vertexShaderCode =
             "attribute vec4 vPosition;" +
+                    "uniform mat4 uMVPMatrix;" +
                     "void main() {" +
-                    "   gl_Position = vPosition;" +
+                    "   gl_Position = vPosition * uMVPMatrix;" +
                     "}";
 
     private final String fragmentShaderCode =
@@ -41,6 +42,8 @@ public class Triangle
     private final int vertexCount = triangleCoordinates.length / COORDINATES_PER_VERTEX;
     private final int vertexStride = COORDINATES_PER_VERTEX * 4;
 
+    private int mMVPMatrixHandle;
+
     public Triangle()
     {
         int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode);
@@ -60,19 +63,21 @@ public class Triangle
         vertexBuffer.position(0);
     }
 
-    public void draw()
+    public void draw(float[] mvpMatrix)
     {
         GLES20.glUseProgram(mProgram);
         mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
         GLES20.glEnableVertexAttribArray(mPositionHandle);
-        GLES20.glVertexAttribPointer(mPositionHandle,                       // index
-                COORDINATES_PER_VERTEX,                                     // size
-                GLES20.GL_FLOAT,                                            // type
-                false,                                                      // normalized
-                vertexStride,                                               // stride
-                vertexBuffer);                                              // pointer
+        GLES20.glVertexAttribPointer(mPositionHandle,   // index
+                COORDINATES_PER_VERTEX, // size
+                GLES20.GL_FLOAT,    // type
+                false,  // normalized
+                vertexStride,   // stride
+                vertexBuffer);  // pointer
         mColorHandle = GLES20.glGetAttribLocation(mProgram, "vColor");
         GLES20.glUniform4fv(mColorHandle, 1, color, 0);
+        mMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
+        GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mvpMatrix, 0);
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
         GLES20.glDisableVertexAttribArray(mPositionHandle);
     }
