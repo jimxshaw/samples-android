8c7296dc8f9b6c50967fdf9ecf9cacf6088edbdf
Jim Shaw <jimxshaw@gmail.com>
2016-05-19 13:07:47 -0500

    implemented some methods within GLRenderer class. added riGraphicTools class boilerplate

--- OpenGL/RealTutorial/app/src/main/java/me/jimmyshaw/realtutorialopengles/GLRenderer.java
+++ OpenGL/RealTutorial/app/src/main/java/me/jimmyshaw/realtutorialopengles/GLRenderer.java
@@ -1,6 +1,7 @@
 package me.jimmyshaw.realtutorialopengles;
 
 import android.content.Context;
+import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 
 import java.nio.FloatBuffer;
@@ -63,6 +64,51 @@ public class GLRenderer implements GLSurfaceView.Renderer
     @Override
     public void onDrawFrame(GL10 gl)
     {
+        // Get the current time.
+        long now = System.currentTimeMillis();
+
+        if (mLastTime > now)
+        {
+            return;
+        }
+
+        // Get the amount of time the last frame took.
+        long elapsed = now - mLastTime;
+
+        // Update our example.
+
+        // Render our example.
+        Render(mtxProjectionAndView);
+
+        // Save the current time to see how long it took <img src="http://androidblog.reindustries.com/wp-includes/images/smilies/icon_smile.gif" alt=":)" class="wp-smiley"> .
+        mLastTime = now;
+    }
+
+    private void Render(float[] m)
+    {
+        // Clear screen and depth buffer, we have set the clear color black.
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+
+        // Get handle to vertex shader's vPosition member.
+        int mPositionHandle = GLES20.glGetAttribLocation(riGraphicTools.sp_SolidColor, "vPosition");
+
+        // Enable generic vertex attribute array.
+        GLES20.glEnableVertexAttribArray(mPositionHandle);
+
+        // Prepare the triangle coordinate data.
+        GLES20.glVertexAttribPointer(mPositionHandle, 3, GLES20.GL_FLOAT, false, 0, vertexBuffer);
+
+        // Get handle to shape's transformation matrix.
+        int mtxHandle = GLES20.glGetUniformLocation(riGraphicTools.sp_SolidColor, "uMVPMatrix");
+
+        // Apply the projection and view transformation.
+        GLES20.glUniformMatrix4fv(mtxHandle, 1, false, m, 0);
+
+        // Draw the triangle.
+        GLES20.glDrawElements(GLES20.GL_TRIANGLES, indices.length, GLES20.GL_UNSIGNED_SHORT, drawListBuffer);
+
+        // Disable vertex array.
+        GLES20.glDisableVertexAttribArray(mPositionHandle);
 
     }
 }
---
+++ OpenGL/RealTutorial/app/src/main/java/me/jimmyshaw/realtutorialopengles/riGraphicTools.java
@@ -0,0 +1,7 @@
+package me.jimmyshaw.realtutorialopengles;
+
+public class riGraphicTools
+{
+    // Program variables.
+    public static int sp_SolidColor;
+}
