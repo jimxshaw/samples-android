1a5f91e1b2bff7fc135292e1ced9ea817a1712ef
Jim Shaw <jimxshaw@gmail.com>
2016-05-19 11:45:11 -0500

    implemented MainActivity's onCreate. created GLSurf class

---
+++ OpenGL/RealTutorial/app/src/main/java/me/jimmyshaw/realtutorialopengles/GLSurf.java
@@ -0,0 +1,12 @@
+package me.jimmyshaw.realtutorialopengles;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+public class GLSurf extends GLSurfaceView
+{
+    public GLSurf(Context context)
+    {
+        super(context);
+    }
+}
--- OpenGL/RealTutorial/app/src/main/java/me/jimmyshaw/realtutorialopengles/MainActivity.java
+++ OpenGL/RealTutorial/app/src/main/java/me/jimmyshaw/realtutorialopengles/MainActivity.java
@@ -3,6 +3,9 @@ package me.jimmyshaw.realtutorialopengles;
 import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -13,7 +16,40 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        // Turn off the window's title bar.
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         super.onCreate(savedInstanceState);
+
+        // Fullscreen mode.
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // We create our SurfaceView for our OpenGL here.
+        glSurfaceView = new GLSurf(this);
+
         setContentView(R.layout.activity_main);
+
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.gamelayout);
+
+        // Attach our surface view to our relative layout from our main layout.
+        RelativeLayout.LayoutParams glParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.MATCH_PARENT);
+        layout.addView(glSurfaceView, glParams);
+
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        glSurfaceView.onPause();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        glSurfaceView.onResume();
     }
 }
