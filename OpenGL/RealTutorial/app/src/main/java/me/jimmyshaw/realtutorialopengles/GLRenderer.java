ab3552cb4fe0ce0f13b3f5609cfbad6a54692452
Jim Shaw <jimxshaw@gmail.com>
2016-05-23 14:07:21 -0500

    finished up Sprite class

--- OpenGL/RealTutorial/app/src/main/java/me/jimmyshaw/realtutorialopengles/GLRenderer.java
+++ OpenGL/RealTutorial/app/src/main/java/me/jimmyshaw/realtutorialopengles/GLRenderer.java
@@ -402,7 +402,7 @@ public class GLRenderer implements GLSurfaceView.Renderer
     // rotating is always done about an origin. What origin though? In our case, we'll do the
     // transformation each frame from the base position. The order we use is scaling, rotating
     // and translating. The result is an image being scaled, rotated around its own midpoint and then
-    // translated to the desired location.
+    // translated to the correct location.
     public class Sprite
     {
         float angle;
@@ -440,6 +440,56 @@ public class GLRenderer implements GLSurfaceView.Renderer
             // Update our location.
             translation.x += deltax;
             translation.y += deltay;
+        }
+
+        public float[] getTransformedVertices()
+        {
+            // Start with scaling.
+            float x1 = base.left * scale;
+            float x2 = base.right * scale;
+            float y1 = base.bottom * scale;
+            float y2 = base.bottom * scale;
+
+            // We detach from our Rect because when rotating, we need separate points. We do so in
+            // OpenGL order.
+            PointF one = new PointF(x1, y2);
+            PointF two = new PointF(x1, y1);
+            PointF three = new PointF(x2, y1);
+            PointF four = new PointF(x2, y2);
+
+            // We create the sin and cos functions once so we don't have to calculate
+            // them each time.
+            float sin = (float) Math.sin(angle);
+            float cos = (float) Math.cos(angle);
+
+            // Then we rotate each point.
+            one.x = x1 * cos - y2 * sin;
+            one.y = x1 * sin + y2 * cos;
+            two.x = x1 * cos - y1 * sin;
+            two.y = x1 * sin + y1 * cos;
+            three.x = x2 * cos - y1 * sin;
+            three.y = x2 * sin + y1 * cos;
+            four.x = x2 * cos - y2 * sin;
+            four.y = x2 * sin + y2 * cos;
+
+            // Finally we translate the sprite to its correct position.
+            one.x += translation.x;
+            one.y += translation.y;
+            two.x += translation.x;
+            two.y += translation.y;
+            three.x += translation.x;
+            three.y += translation.y;
+            four.x += translation.x;
+            four.y += translation.y;
+
+            // We return our float array of vertices.
+            return new float[]
+                    {
+                            one.x, one.y, 0.0f,
+                            two.x, two.y, 0.0f,
+                            three.x, three.y, 0.0f,
+                            four.x, four.y, 0.0f
+                    };
         }
     }
 }
