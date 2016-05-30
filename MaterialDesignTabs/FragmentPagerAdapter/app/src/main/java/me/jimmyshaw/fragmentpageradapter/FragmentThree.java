26f3ae8e16258c9b8594be4a6a95945eaa65a38f
Jim Shaw <jimxshaw@gmail.com>
2016-05-30 17:10:06 -0500

    added FragmentPagerAdapter sample

---
+++ MaterialDesignTabs/FragmentPagerAdapter/app/src/main/java/me/jimmyshaw/fragmentpageradapter/FragmentThree.java
@@ -0,0 +1,24 @@
+package me.jimmyshaw.fragmentpageradapter;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentThree extends Fragment {
+
+    public FragmentThree() {
+        Log.i("Fragment", "Fragment Three Created");
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_three, container, false);
+    }
+}
+
---
+++ MaterialDesignTabs/FragmentPagerAdapter/app/src/main/java/me/jimmyshaw/fragmentpageradapter/FragmentTwo.java
@@ -0,0 +1,24 @@
+package me.jimmyshaw.fragmentpageradapter;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentTwo extends Fragment {
+
+    public FragmentTwo() {
+        Log.i("Fragment", "Fragment Two Created");
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_two, container, false);
+    }
+}
+
