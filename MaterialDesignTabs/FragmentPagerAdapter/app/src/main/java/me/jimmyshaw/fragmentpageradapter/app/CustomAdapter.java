61e234065fe245420925f35c7e08a4f021d4733a
Jim Shaw <jimxshaw@gmail.com>
2016-05-30 17:43:23 -0500

    added comments about the FragmentPagerAdapter

--- MaterialDesignTabs/FragmentPagerAdapter/app/src/main/java/me/jimmyshaw/fragmentpageradapter/app/CustomAdapter.java
+++ MaterialDesignTabs/FragmentPagerAdapter/app/src/main/java/me/jimmyshaw/fragmentpageradapter/app/CustomAdapter.java
@@ -11,6 +11,11 @@ import me.jimmyshaw.fragmentpageradapter.fragments.FragmentSix;
 import me.jimmyshaw.fragmentpageradapter.fragments.FragmentThree;
 import me.jimmyshaw.fragmentpageradapter.fragments.FragmentTwo;
 
+// When a Fragment's destroyItem method is called, it isn't actually destroyed but is detached
+// instead. All fragments are kept in memory. If the number of fragments are increased significantly
+// then a high probability of an out-of-memory exception will occur and our app will crash. This
+// is a disadvantage of using a FragmentPagerAdapter. For apps with a large number of fragments,
+// use a FragmentStatePagerAdapter instead. 
 public class CustomAdapter extends FragmentPagerAdapter {
 
     private final int ITEMS = 6;
