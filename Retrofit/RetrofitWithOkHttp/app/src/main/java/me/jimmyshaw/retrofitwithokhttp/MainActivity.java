5850d459edcb47284e3c005cd56c015961a43242
Jim Shaw <jimxshaw@gmail.com>
2016-06-01 11:04:04 -0500

    implemented ServiceGenerator with GitHubService and debugged the response

--- Retrofit/RetrofitWithOkHttp/app/src/main/java/me/jimmyshaw/retrofitwithokhttp/MainActivity.java
+++ Retrofit/RetrofitWithOkHttp/app/src/main/java/me/jimmyshaw/retrofitwithokhttp/MainActivity.java
@@ -2,6 +2,13 @@ package me.jimmyshaw.retrofitwithokhttp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create a simple REST adapter that points to GitHub's api.
+        GitHubService service = ServiceGenerator.createService(GitHubService.class);
+
+        // Retrieve and print a list of repositories for a particular user.
+        Call<List<GitHubRepo>> call = service.reposForUser("lvwiuhv");
+        call.enqueue(new Callback<List<GitHubRepo>>() {
+            @Override
+            public void onResponse(Call<List<GitHubRepo>> call, Response<List<GitHubRepo>> response) {
+                if (response.isSuccessful()) {
+                    for (GitHubRepo repo : response.body()) {
+                        Log.d("Repo: ", repo.getName() + " (ID: " + repo.getId() + ")");
+                    }
+                }
+                else {
+                    Log.e("Request failure: ", "Could not retrieve GitHub repositories");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<GitHubRepo>> call, Throwable t) {
+                Log.e("Error retrieving repos", t.getMessage());
+            }
+        });
     }
 }
