f657c9d50f54ad3cc3b29d664c880660774f37dc
Jim Shaw <jimxshaw@gmail.com>
2016-06-01 22:17:15 -0500

    defined TabsTextAdapter, utilized the data from TabsText activity and output the data using a viewpager

--- MaterialDesignTabs/MaterialDesignTabs/app/src/main/java/me/jimmyshaw/materialdesigntabs/tabs/TabsText.java
+++ MaterialDesignTabs/MaterialDesignTabs/app/src/main/java/me/jimmyshaw/materialdesigntabs/tabs/TabsText.java
@@ -3,6 +3,7 @@ package me.jimmyshaw.materialdesigntabs.tabs;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -10,14 +11,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import me.jimmyshaw.materialdesigntabs.R;
+import me.jimmyshaw.materialdesigntabs.adapters.TabsTextAdapter;
 import me.jimmyshaw.materialdesigntabs.fragments.FragmentOne;
 import me.jimmyshaw.materialdesigntabs.fragments.FragmentThree;
 import me.jimmyshaw.materialdesigntabs.fragments.FragmentTwo;
 
 public class TabsText extends AppCompatActivity {
 
-    private List<Fragment> fragmentList = new ArrayList<>();
-    private List<String> titleList = new ArrayList<>();
+    private ViewPager mViewPager;
+
+    private List<Fragment> mFragmentList = new ArrayList<>();
+    private List<String> mTitleList = new ArrayList<>();
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -27,12 +31,18 @@ public class TabsText extends AppCompatActivity {
         initialize();
 
         prepareDataResource();
+
+        TabsTextAdapter adapter = new TabsTextAdapter(getSupportFragmentManager(), mFragmentList, mTitleList);
+
+        mViewPager.setAdapter(adapter);
     }
 
     private void initialize() {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Tabs with Text Example");
+
+        mViewPager = (ViewPager) findViewById(R.id.view_pager);
     }
 
     private void prepareDataResource() {
@@ -42,7 +52,7 @@ public class TabsText extends AppCompatActivity {
     }
 
     private void addData(Fragment fragment, String title) {
-        fragmentList.add(fragment);
-        titleList.add(title);
+        mFragmentList.add(fragment);
+        mTitleList.add(title);
     }
 }
