db4534aea2f375a94a696e81e636cd10b123f39d
Jim Shaw <jimxshaw@gmail.com>
2016-06-23 13:42:45 -0400

    implemented queueThumbnail method of ThumbnailDownloader class

--- PhotoGallery/app/src/main/java/me/jimmyshaw/photogallery/ThumbnailDownloader.java
+++ PhotoGallery/app/src/main/java/me/jimmyshaw/photogallery/ThumbnailDownloader.java
@@ -1,7 +1,11 @@
 package me.jimmyshaw.photogallery;
 
+import android.os.Handler;
 import android.os.HandlerThread;
 import android.util.Log;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /*
     The user, PhotoGalleryFragment, will use this class. It'll need to use some object to identify
@@ -12,8 +16,17 @@ import android.util.Log;
 public class ThumbnailDownloader<T> extends HandlerThread {
 
     private static final String TAG = "ThumbnailDownloader";
+    // This int will be used to identify messages as download requests. ThumbnailDownloader will set
+    // this as the what field on any new download messages it creates.
+    private static final int MESSAGE_DOWNLOAD = 0;
 
     private boolean mHasQuit = false;
+    // This handler will store a reference to the Handler responsible for queueing download requests
+    // as messages on to the ThumbnailDownloader background thread. This handler will also be in
+    // charge of processing download request messages when they are pulled off the queue.
+    private Handler mRequestHandler;
+    // This map is a thread-safe version of hashmap.
+    private ConcurrentMap<T, String> mRequestMap = new ConcurrentHashMap<>();
 
     public ThumbnailDownloader() {
         super(TAG);
@@ -28,6 +41,15 @@ public class ThumbnailDownloader<T> extends HandlerThread {
     // PhotoAdapter will call this method in its onBindViewHolder implementation.
     public void queueThumbnail(T target, String url) {
         Log.i(TAG, "Got a URL: " + url);
+
+        if (url == null) {
+            mRequestMap.remove(target);
+        }
+        else {
+            mRequestMap.put(target, url);
+            mRequestHandler.obtainMessage(MESSAGE_DOWNLOAD, target).sendToTarget();
+        }
+
     }
 
 }
