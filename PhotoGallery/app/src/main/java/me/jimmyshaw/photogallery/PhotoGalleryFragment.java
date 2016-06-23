3ecb49a3b0eabedcc8a2a2085e22123c40e65c5b
Jim Shaw <jimxshaw@gmail.com>
2016-06-22 20:43:15 -0400

    added PhotoGallery project boilerplate

---
+++ PhotoGallery/app/src/main/java/me/jimmyshaw/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,16 @@
+package me.jimmyshaw.photogallery;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+
+public class PhotoGalleryFragment extends Fragment {
+
+    private RecyclerView mPhotoRecyclerView;
+
+    public static PhotoGalleryFragment newInstance() {
+        return new PhotoGalleryFragment();
+    }
+
+    
+
+}
