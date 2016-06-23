c8d650f53d76f1962f2d9531559659bcf26f701e
Jim Shaw <jimxshaw@gmail.com>
2016-06-23 10:10:12 -0400

    added GalleryItem model class

--- PhotoGallery/app/src/main/java/me/jimmyshaw/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/app/src/main/java/me/jimmyshaw/photogallery/PhotoGalleryFragment.java
@@ -5,12 +5,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import java.io.IOException;
 
 public class PhotoGalleryFragment extends Fragment {
 
@@ -45,14 +42,7 @@ public class PhotoGalleryFragment extends Fragment {
 
         @Override
         protected Void doInBackground(Void... voids) {
-            try {
-                String result = new FlickrFetchr().getUrlString("https://www.bignerdranch.com");
-
-                Log.i(TAG, "Fetched contents of URL: " + result);
-            }
-            catch (IOException e) {
-                Log.e(TAG, "Failed to fetch URL: ", e);
-            }
+            new FlickrFetchr().fetchItems();
 
             return null;
         }
