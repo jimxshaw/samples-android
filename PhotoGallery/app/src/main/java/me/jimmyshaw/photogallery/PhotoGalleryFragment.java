9077929628df868229f7a1bdfba690e1585f8554
Jim Shaw <jimxshaw@gmail.com>
2016-06-23 16:41:42 -0400

    added search menu layout. applied layout to PhotoGalleryFragment

--- PhotoGallery/app/src/main/java/me/jimmyshaw/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/app/src/main/java/me/jimmyshaw/photogallery/PhotoGalleryFragment.java
@@ -11,6 +11,8 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -38,6 +40,7 @@ public class PhotoGalleryFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        setHasOptionsMenu(true);
 
         new FetchItemsTask().execute();
 
@@ -82,6 +85,13 @@ public class PhotoGalleryFragment extends Fragment {
         super.onDestroyView();
         mThumbnailDownloader.clearQueue();
     }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
+        super.onCreateOptionsMenu(menu, menuInflater);
+        menuInflater.inflate(R.menu.fragment_photo_gallery, menu);
+    }
+
 
     private void setupAdapter() {
         if (isAdded()) {
