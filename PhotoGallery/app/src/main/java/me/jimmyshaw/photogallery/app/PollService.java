3c2c0a86be9700411187273c0315a9ab2ac0fcb1
Jim Shaw <jimxshaw@gmail.com>
2016-06-23 20:53:52 -0400

    added isNetworkAvailableAndConnected method and added access network state permission to manifest.

--- PhotoGallery/app/src/main/java/me/jimmyshaw/photogallery/app/PollService.java
+++ PhotoGallery/app/src/main/java/me/jimmyshaw/photogallery/app/PollService.java
@@ -3,6 +3,7 @@ package me.jimmyshaw.photogallery.app;
 import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.util.Log;
 
 /*
@@ -27,6 +28,18 @@ public class PollService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        if (!isNetworkAvailableAndConnected()) {
+            return;
+        }
         Log.i(TAG, "Received an intent: " + intent);
+    }
+
+    private boolean isNetworkAvailableAndConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+
+        boolean isNetworkAvailable = cm.getActiveNetworkInfo() != null;
+        boolean isNetworkConnected = isNetworkAvailable && cm.getActiveNetworkInfo().isConnected();
+
+        return isNetworkConnected;
     }
 }
