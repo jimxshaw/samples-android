b542c9cbbe752de08c3e1dc98b17f5201294d0b5
Jim Shaw <jimxshaw@gmail.com>
2016-06-25 14:48:56 -0500

    added tool bar layout. included the toolbar in main activity layout

--- DropBucketList/DropBucketList/app/src/main/java/me/jimmyshaw/dropbucketlist/ActivityMain.java
+++ DropBucketList/DropBucketList/app/src/main/java/me/jimmyshaw/dropbucketlist/ActivityMain.java
@@ -1,13 +1,18 @@
 package me.jimmyshaw.dropbucketlist;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class ActivityMain extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
