7071d925d4a0f72bf37970fc2f920222c3570e11
Jim Shaw <jimxshaw@gmail.com>
2016-06-28 12:49:41 -0500

    switched the background png from the recycler view widget to the parent main activity layout. Created custom recycler view class and partially implemented it.

---
+++ DropBucketList/DropBucketList/app/src/main/java/me/jimmyshaw/dropbucketlist/widgets/DropRecyclerView.java
@@ -0,0 +1,75 @@
+package me.jimmyshaw.dropbucketlist.widgets;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+// This is a custom recycler view class that behaves differently from the normal recycler view.
+public class DropRecyclerView extends RecyclerView {
+
+    // Our app's main activity includes a recycler view and a tool bar. The vision is that the
+    // recycler view and tool bar will only appear when the recycler view has data to display.
+    // If there's no data then the main UI will only show our app's symbol and the Add a drop button.
+    // To do this, we use the abstract class called AdapterDataObserver with methods that will
+    // each check whether or not the recycler view contains data.
+    private AdapterDataObserver mAdapterDataObserver = new AdapterDataObserver() {
+        @Override
+        public void onChanged() {
+
+        }
+
+        @Override
+        public void onItemRangeChanged(int positionStart, int itemCount) {
+
+        }
+
+        @Override
+        public void onItemRangeChanged(int positionStart, int itemCount, Object payload) {
+
+        }
+
+        @Override
+        public void onItemRangeInserted(int positionStart, int itemCount) {
+
+        }
+
+        @Override
+        public void onItemRangeRemoved(int positionStart, int itemCount) {
+
+        }
+
+        @Override
+        public void onItemRangeMoved(int fromPosition, int toPosition, int itemCount) {
+
+        }
+    };
+
+    // Typical constructor to initialize a recycler view from code. The other two constructors are
+    // to initialize the constructor from xml.
+    public DropRecyclerView(Context context) {
+        super(context);
+    }
+
+    public DropRecyclerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DropRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public void setAdapter(Adapter adapter) {
+        super.setAdapter(adapter);
+
+        // Whenever our recycler view has its adapter set, we'll modify the adapter's usual
+        // behavior by having it register our adapter data observer. Every time set adapter is called
+        // our adapter changes and we fire off the observer's onChange method that we defined above.
+        if (adapter != null) {
+            adapter.registerAdapterDataObserver(mAdapterDataObserver);
+        }
+
+        mAdapterDataObserver.onChanged();
+    }
+}
