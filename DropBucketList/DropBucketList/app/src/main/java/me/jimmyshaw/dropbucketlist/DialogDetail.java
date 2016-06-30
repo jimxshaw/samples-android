91c0dbd9f6a574f9d4b01ecae1439b46724484b3
Jim Shaw <jimxshaw@gmail.com>
2016-06-30 12:42:25 -0500

    added boilerplate DialogDetail fragment

---
+++ DropBucketList/DropBucketList/app/src/main/java/me/jimmyshaw/dropbucketlist/DialogDetail.java
@@ -0,0 +1,46 @@
+package me.jimmyshaw.dropbucketlist;
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class DialogDetail extends DialogFragment {
+
+    private ImageButton mButtonClose;
+    private Button mButtonCompleted;
+
+    // The widgets on the detail dialog can have their click functions dictated by a
+    // single on click listener that differentiates between the widgets by their ids.
+    private View.OnClickListener mButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.button_mark_completed:
+                    // TODO: Handle the action to mark the item as completed.
+                    break;
+                default:
+                    break;
+            }
+            dismiss();
+        }
+    };
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.dialog_detail, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mButtonClose = (ImageButton) view.findViewById(R.id.button_close);
+        mButtonClose.setOnClickListener(mButtonClickListener);
+        mButtonCompleted = (Button) view.findViewById(R.id.button_mark_completed);
+        mButtonCompleted.setOnClickListener(mButtonClickListener);
+    }
+}
