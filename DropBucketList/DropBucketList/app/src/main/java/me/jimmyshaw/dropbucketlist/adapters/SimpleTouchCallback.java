79aab40649a96525f4376a41bd606a935e51dc5b
Jim Shaw <jimxshaw@gmail.com>
2016-07-02 15:53:31 -0500

    added conditions to onSwiped, onChildDraw and onChildDrawOver methods in SimpleTouchCallback to only allow swiping on DropHolder row items

--- DropBucketList/DropBucketList/app/src/main/java/me/jimmyshaw/dropbucketlist/adapters/SimpleTouchCallback.java
+++ DropBucketList/DropBucketList/app/src/main/java/me/jimmyshaw/dropbucketlist/adapters/SimpleTouchCallback.java
@@ -1,5 +1,6 @@
 package me.jimmyshaw.dropbucketlist.adapters;
 
+import android.graphics.Canvas;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 
@@ -40,6 +41,28 @@ public class SimpleTouchCallback extends ItemTouchHelper.Callback {
     public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
         // This method has to communicate with the recycler view adapter in order to delete the row
         // item that has been swiped.
-        mSwipeListener.onSwipe(viewHolder.getAdapterPosition());
+        // This if conditional is needed so that only row items can be swiped away. Otherwise, the footer
+        // or the no items layout can be swiped away. But this isn't enough as we'll have to override
+        // two methods onChildDraw and onChildDrawOver as their default behavior is what governs
+        // the swiping in the x direction or y direction.
+        if (viewHolder instanceof AdapterDrops.DropHolder) {
+            mSwipeListener.onSwipe(viewHolder.getAdapterPosition());
+        }
+    }
+
+    // Add the same if conditional to the super statements of these two method to allow only DropHolder
+    // row items to be swiped.
+    @Override
+    public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        if (viewHolder instanceof AdapterDrops.DropHolder) {
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+    }
+
+    @Override
+    public void onChildDrawOver(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        if (viewHolder instanceof AdapterDrops.DropHolder) {
+            super.onChildDrawOver(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
     }
 }
