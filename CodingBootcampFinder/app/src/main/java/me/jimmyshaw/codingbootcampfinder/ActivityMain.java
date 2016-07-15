4731b213c2150728891cd2386b8ceeb72fdef755
Jim Shaw <jimxshaw@gmail.com>
2016-07-15 14:48:47 -0500

    incorporated GoogleApiClient interfaces. wrote startLocationServies method.

--- CodingBootcampFinder/app/src/main/java/me/jimmyshaw/codingbootcampfinder/ActivityMain.java
+++ CodingBootcampFinder/app/src/main/java/me/jimmyshaw/codingbootcampfinder/ActivityMain.java
@@ -1,31 +1,106 @@
 package me.jimmyshaw.codingbootcampfinder;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 import me.jimmyshaw.codingbootcampfinder.fragments.FragmentMain;
 
-public class ActivityMain extends FragmentActivity {
+public class ActivityMain extends FragmentActivity implements GoogleApiClient.OnConnectionFailedListener,
+        GoogleApiClient.ConnectionCallbacks,
+        LocationListener {
 
-    FragmentMain mFragmentMain;
+    private FragmentMain mFragmentMain;
+
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
+
         loadFragment();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        // This method is called by our Google API services client after a connection has been
+        // established. Where the connection actually happens is within this activity's onStart and
+        // the disconnection is within this activity's onStop.
+        // However, we only start location services if the user gives permission.
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 
     private void loadFragment() {
         mFragmentMain = (FragmentMain) getSupportFragmentManager()
-                                        .findFragmentById(R.id.frame_layout_container_main);
+                .findFragmentById(R.id.frame_layout_container_main);
 
         if (mFragmentMain == null) {
             mFragmentMain = mFragmentMain.newInstance();
-            getSupportFragmentManager().beginTransaction().add(R.id.frame_layout_container_main, mFragmentMain).commit();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.frame_layout_container_main, mFragmentMain)
+                    .commit();
         }
     }
 
+    public void startLocationServices() {
+        // After the Google API Client is connected, we start our location services.
+        Log.d("ActivityMain", "startLocationServices called");
+
+        // We use a try-catch because location services need explicit permission approval from the
+        // user. If permission is denied then the code drops to the catch statement where, for example,
+        // we show a dialog saying we can't get the location unless user permission is granted.
+        try {
+            // Setting the priority as low power means we're wanting city-level accuracy.
+            LocationRequest request = LocationRequest.create()
+                    .setPriority(LocationRequest.PRIORITY_LOW_POWER);
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, request, this);
+        }
+        catch (SecurityException ex) {
+            Log.d("ActivityMain", ex.toString());
+            ex.printStackTrace();
+        }
+    }
 }
