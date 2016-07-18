b19c63394975e062a58404128dc81594a0ba8782
Jim Shaw <jimxshaw@gmail.com>
2016-07-18 16:23:22 -0500

    wrote setupRecyclerView method.

--- CodingBootcampFinder/app/src/main/java/me/jimmyshaw/codingbootcampfinder/adapters/CampAdapter.java
+++ CodingBootcampFinder/app/src/main/java/me/jimmyshaw/codingbootcampfinder/adapters/CampAdapter.java
@@ -1,5 +1,40 @@
 package me.jimmyshaw.codingbootcampfinder.adapters;
 
-public class CampsAdapter {
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
+import me.jimmyshaw.codingbootcampfinder.models.Camp;
+
+public class CampAdapter extends RecyclerView.Adapter<CampAdapter.CampHolder> {
+
+    private ArrayList<Camp> mCamps;
+
+    public CampAdapter(ArrayList<Camp> camps) {
+        mCamps = camps;
+    }
+
+    @Override
+    public CampHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(CampHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCamps.size();
+    }
+
+    public class CampHolder extends RecyclerView.ViewHolder {
+
+        public CampHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
--- CodingBootcampFinder/app/src/main/java/me/jimmyshaw/codingbootcampfinder/fragments/FragmentCampsList.java
+++ CodingBootcampFinder/app/src/main/java/me/jimmyshaw/codingbootcampfinder/fragments/FragmentCampsList.java
@@ -3,13 +3,24 @@ package me.jimmyshaw.codingbootcampfinder.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 import me.jimmyshaw.codingbootcampfinder.R;
+import me.jimmyshaw.codingbootcampfinder.adapters.CampAdapter;
+import me.jimmyshaw.codingbootcampfinder.models.Camp;
+import me.jimmyshaw.codingbootcampfinder.services.DataService;
 
 public class FragmentCampsList extends Fragment {
+
+    private RecyclerView mRecyclerView;
+
+    private ArrayList<Camp> mCamps;
 
     public FragmentCampsList() {
         // Required empty public constructor
@@ -25,6 +36,8 @@ public class FragmentCampsList extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        mCamps = DataService.getInstance().getCampLocationsWithinTenMilesOfZipCode(75025);
+
     }
 
     @Override
@@ -33,7 +46,20 @@ public class FragmentCampsList extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_camps_list, container, false);
 
+        setupRecyclerView(view);
+
         return view;
+    }
+
+    private void setupRecyclerView(View view) {
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setAdapter(new CampAdapter(mCamps));
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+
     }
 
 }
