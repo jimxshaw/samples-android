dd669ee4ebabd1adc367cb46acbaabccc62ce07d
Jim Shaw <jimxshaw@gmail.com>
2022-02-23 18:03:03 -0600

    Add boilerplate layout, fragment for About Page.

---
+++ GlobalTourDefaultStyle/app/src/main/java/me/jimmyshaw/globaltour/info/AboutFragment.kt
@@ -0,0 +1,21 @@
+package me.jimmyshaw.globaltour.info
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import me.jimmyshaw.globaltour.R
+
+class AboutFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_about_app, container, false)
+
+        return view
+    }
+}
